2bd26a797295ebac4e8e70d12353367d3b363be7
eStrau <ellestrauss01@gmail.com>
2024-04-11 13:03:54 -0400

    setting up ui

--- InDepth/app.R
+++ InDepth/app.R
@@ -7,29 +7,35 @@
 #    https://shiny.posit.co/
 #
 
+# Libraries Needed:
 library(shiny)
+library(bslib)
+library(googlesheets4)
+library(tidyverse)
+library(magrittr)
+library(stringr)
+library(lubridate)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- page_sidebar(
+  title = "In Depth",
+  sidebar = sidebar("sidebar"),
+  # Upload Attendance Section
+  "Upload Attendance",
+  layout_columns(col_widths = 6, 
+                 textInput('master',"Enter Master Attendance Workbook URL:"),
+                 textInput('event',"Enter Event Attendance Sheet URL:"),
+  ),
+  layout_columns(
+    textInput('eventName',"Enter Event Name:"),
+    dateInput('eventDate', "Enter the Event Date:"),
+    selectInput('eventType', "Select Event Type", choices=list("Social"=1, "Career"=2, "Workshop"=3, "GBM"=4, "Technical"=5)),
+  ),
+  submitButton("Submit Attendance"),
+  # Other Sections
+  card("Unique Members Data"),
+  card("Visuals"), 
+  card("Emails to List Server")
 )
 
 # Define server logic required to draw a histogram
