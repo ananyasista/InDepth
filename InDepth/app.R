000521d1bd6ae1cf674f3cd7ae9068708805c4ad
Ananya Sista <sistaa@ufl.edu>
2024-04-18 16:31:29 -0400

    added append to master, find errors, add to uniques and update attendance counts

--- InDepth/app.R
+++ InDepth/app.R
@@ -15,6 +15,11 @@ library(tidyverse)
 library(magrittr)
 library(stringr)
 library(lubridate)
+library(shinyjqui)
+
+# global uses
+eventTypes <- list("Social", "Career", "Workshop", "GBM", "Technical")
+uni_col <- c("First Name", "Last Name",	"UFID (Ex: 12345678)",	"Year",	"Major",	"Total",	"Social",	"Career",	"Technical", "GBM",	"Workshop")
 
 # Define UI for application that draws a histogram
 ui <- page_sidebar(
@@ -22,25 +27,114 @@ ui <- page_sidebar(
   sidebar = sidebar("sidebar"),
   # Upload Attendance Section
   "Upload Attendance",
-  layout_columns(col_widths = 6, 
-                 textInput('master',"Enter Master Attendance Workbook URL:"),
-                 textInput('event',"Enter Event Attendance Sheet URL:"),
+  layout_columns(textInput('workbook',"Enter Attendnace Workbook URL:")
+  ),
+  layout_columns(col_widths=6,
+    textInput('masterName',"Enter Master Worksheet Name:"),
+    textInput('eventName',"Enter Event Worksheet Name:"),
+    textInput('uniName',"Enter Unique Members Worksheet Name:"),
+    textInput('errors',"Enter Error Worksheet Name:"),
   ),
   layout_columns(
-    textInput('eventName',"Enter Event Name:"),
-    dateInput('eventDate', "Enter the Event Date:"),
-    selectInput('eventType', "Select Event Type", choices=list("Social"=1, "Career"=2, "Workshop"=3, "GBM"=4, "Technical"=5)),
+    selectInput('eventType', "Select Event Type", choices=eventTypes),
   ),
-  submitButton("Submit Attendance"),
+  actionButton("submit", "Submit!"),
   # Other Sections
-  card("Unique Members Data"),
+  card("Unique Members Data"), 
   card("Visuals"), 
   card("Emails to List Server")
 )
 
+# append data to master
+writeMaster <- function(url, e, mName, eType, eName){
+  sheet_append(url, data.frame(eType, eName), sheet=mName)
+  sheet_append(url, e, sheet=mName)
+  sheet_append(url, data.frame(""), sheet=mName)
+  
+}
+
+# find errors with UFID
+findErrors <- function(url, e, errName){
+  hasErr <- FALSE
+  for(r in 1:nrow(e)) {
+    if(length(str_view(e$`UFID  (Ex: 12345678)`[r], "\\d{8}")) != 1) {
+      incorrect <- e[r,]
+      sheet_append(url, incorrect, sheet=errName)
+      hasErr <- TRUE
+    }
+  }
+  
+  return(hasErr)
+}
+
+# create Uniques sheet
+parseUniques <- function(url, uName, u, e){
+  test <- data.frame(NA, NA, NA, NA, NA, 0, 0, 0, 0, 0, 0)
+  colnames(test) <- uni_col
+  range_write(url, test, sheet=uName, range = "A1", col_names =TRUE)
+  for(n in 1:nrow(e)){
+    index <- which(u$`UFID (Ex: 12345678)`== e[[n, 'UFID  (Ex: 12345678)']])
+    if(length(index) == 0){
+      member <- data.frame(e[n, 'First Name'], 
+                                   e[n, 'Last Name'], 
+                                   e[n, 'UFID  (Ex: 12345678)'],
+                                   e[n, 'Year'],
+                                   e[n, 'Major'],
+                                   0,
+                                   0,
+                                   0,
+                                   0,
+                                   0,
+                                   0)
+      colnames(member) <- uni_col
+      member['Total'] = 1
+      member[e$type[[n]]] = 1
+      sheet_append(url, member, sheet=uName)
+    } else {
+      member <- data.frame(u[index[1], 'First Name'], 
+                           u[index[1], 'Last Name'], 
+                           u[index[1], 'UFID (Ex: 12345678)'],
+                           u[index[1], 'Year'],
+                           u[index[1], 'Major'],
+                           u[index[1], 'Total'],
+                           u[index[1], 'Social'],
+                           u[index[1], 'Career'],
+                           u[index[1], 'Technical'],
+                           u[index[1], 'GBM'],
+                           u[index[1], 'Workshop'])
+      colnames(member) <- uni_col
+      member['Total'] = member['Total'] + 1
+      member[e$type[[n]]] = member[e$type[[n]]] + 1
+      range_write(url, member, sheet=uName, range=paste0("A",1+index[1]), col_names=FALSE)
+    }
+    
+  }
+  
+}
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+  # on submission of intial URLs
+    observeEvent(input$submit, {
+      master <- read_sheet(input$workbook, sheet=input$masterName)
+      event <- read_sheet(input$workbook, sheet=input$eventName)
+      unique <- read_sheet(input$workbook, sheet=input$uniName)
+      if(findErrors(input$workbook, event, input$errors)){
+        showNotification("There are errors with your UFIDs! Please correct them and retry. UFIDs should contain only numeric values and be 8 digits long.", duration=10)
+        
+      } else {
+        event <- event %>% mutate(type=input$eventType) %>% relocate(type, .before=Timestamp) %>% mutate(`UFID  (Ex: 12345678)`=as.character(`UFID  (Ex: 12345678)`))
+        writeMaster(input$workbook, event, "TA", input$eventType, "ex")
+        parseUniques(input$workbook, input$uniName, unique, event)
+        master <- read_sheet(input$workbook, sheet=input$masterName)
+        master
+      }
+      
+      
+    })
+  
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
