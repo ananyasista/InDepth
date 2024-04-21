b89ead21a17246cbf2628e6c75e4ec90e356dd2a
Ananya Sista <sistaa@ufl.edu>
2024-04-21 17:49:50 -0400

    added graph, listserv email, and top members functionality

--- InDepth/app.R
+++ InDepth/app.R
@@ -16,38 +16,82 @@ library(magrittr)
 library(stringr)
 library(lubridate)
 library(shinyjqui)
+library(ggplot2)
 
 # global uses
-eventTypes <- list("Social", "Career", "Workshop", "GBM", "Technical")
-uni_col <- c("First Name", "Last Name",	"UFID (Ex: 12345678)",	"Year",	"Major",	"Total",	"Social",	"Career",	"Technical", "GBM",	"Workshop")
-
+eventTypes <- list("Social", "Career", "Workshop", "GBM", "Technical", "IM")
+uni_col <- c("First Name", "Last Name",	"UFID (Ex: 12345678)",	"Year",	"Major",	"Total",	"Social",	"Career",	"Technical", "GBM",	"Workshop", "IM")
+metricTypes <- list("Total", "Career", "Social", "Technical", "GBM", "Workshop", "IM")
+metricTypesc <- c("Total", "Career", "Social", "Technical", "GBM", "Workshop", "IM")
 # Define UI for application that draws a histogram
-ui <- page_sidebar(
-  title = "In Depth",
-  sidebar = sidebar("sidebar"),
+ui <- fluidPage(
+  tags$style(type = "text/css", "#mainPanel { margin-left: 20px; margin-right: 20px; }"),
+  titlePanel("In Depth"),
+  tags$h3("Upload Attendance"),
   # Upload Attendance Section
-  "Upload Attendance",
-  layout_columns(textInput('workbook',"Enter Attendnace Workbook URL:")
+  mainPanel(
+    
+  fluidRow(
+    layout_columns(
+      textInput('workbook',"Enter Attendnace Workbook URL:")
+    )
   ),
-  layout_columns(col_widths=6,
-    textInput('masterName',"Enter Master Worksheet Name:"),
-    textInput('eventName',"Enter Event Worksheet Name:"),
-    textInput('uniName',"Enter Unique Members Worksheet Name:"),
-    textInput('errors',"Enter Error Worksheet Name:"),
+  fluidRow(
+      layout_columns(
+                     textInput('masterName',"Enter Master Worksheet Name:"),
+                     textInput('eventName',"Enter Event Worksheet Name:"),
+                     
+      )
   ),
-  layout_columns(
-    selectInput('eventType', "Select Event Type", choices=eventTypes),
+  fluidRow(
+    layout_columns(
+                   textInput('uniName',"Enter Unique Members Worksheet Name:"),
+                   textInput('errors',"Enter Error Worksheet Name:"),
+                   
+    )
   ),
-  actionButton("submit", "Submit!"),
+  fluidRow(
+    layout_columns(
+      selectInput('eventType', "Select Event Type", choices=eventTypes),
+    )
+  ),
+  fluidRow(
+    actionButton("submit", "Submit!", width=300),
+  ),
+), br(),
+mainPanel(
+  fluidRow(br(),tags$h3("ListServ Emails"),
+           actionButton("emails", "Create ListServ Email List"), br(),
+  ),
+),
+mainPanel(
+  fluidRow(tags$h3("Generate Top Members"),
+           numericInput("memAmt", "Number of Top Members", 5, min=0), br(),
+           tableOutput("table")
+  ),
+),
+mainPanel(
+  fluidRow(
+    tags$h3("Event Demographic Charts"),
+    layout_columns(
+      selectInput('metrics', "Select Metric Category", choices=metricTypes),
+    ),
+    plotOutput("graph"),
+    plotOutput("graph2"),
+    plotOutput("graph3"),
+  ), 
+)
+  
+  
   # Other Sections
-  card("Unique Members Data"), 
-  card("Visuals"), 
-  card("Emails to List Server")
+  
+
+
 )
 
 # append data to master
 writeMaster <- function(url, e, mName, eType, eName){
-  sheet_append(url, data.frame(eType, eName), sheet=mName)
+  sheet_append(url, data.frame(eType, eName, paste0("Attedance Count: ", nrow(e))), sheet=mName)
   sheet_append(url, e, sheet=mName)
   sheet_append(url, data.frame(""), sheet=mName)
   
@@ -69,7 +113,7 @@ findErrors <- function(url, e, errName){
 
 # create Uniques sheet
 parseUniques <- function(url, uName, u, e){
-  test <- data.frame(NA, NA, NA, NA, NA, 0, 0, 0, 0, 0, 0)
+  test <- data.frame(NA, NA, NA, NA, NA, 0, 0, 0, 0, 0, 0, 0)
   colnames(test) <- uni_col
   range_write(url, test, sheet=uName, range = "A1", col_names =TRUE)
   for(n in 1:nrow(e)){
@@ -80,6 +124,7 @@ parseUniques <- function(url, uName, u, e){
                                    e[n, 'UFID  (Ex: 12345678)'],
                                    e[n, 'Year'],
                                    e[n, 'Major'],
+                                   0,
                                    0,
                                    0,
                                    0,
@@ -101,7 +146,8 @@ parseUniques <- function(url, uName, u, e){
                            u[index[1], 'Career'],
                            u[index[1], 'Technical'],
                            u[index[1], 'GBM'],
-                           u[index[1], 'Workshop'])
+                           u[index[1], 'Workshop'],
+                           u[index[1], 'IM'])
       colnames(member) <- uni_col
       member['Total'] = member['Total'] + 1
       member[e$type[[n]]] = member[e$type[[n]]] + 1
@@ -112,9 +158,65 @@ parseUniques <- function(url, uName, u, e){
   
 }
 
+topMem <- function(amount, u){
+  top <- u %>% arrange(desc(Total))
+  return (top %>% slice_head(n=amount) %>% select("First Name", "Last Name", "Total"))
+}
+
+emailList <- function(mast, url){
+  colnames(mast) <- c("Event Type", "Timestamp", "First Name", "Last Name", "UFID", "Hear", "Return", "Email", "Pic", "Year", "Major")
+  
+  emailList <- mast %>% filter(!is.na(Email)) %>% distinct(Email) %>% inner_join(mast, by = "Email") %>%  select(Email, `First Name`, `Last Name`) %>% distinct(Email, `First Name`, `Last Name`)
+  #colnames(emailList) <- c("email", "first name", "last name")
+  sheet_write(emailList, url, sheet="ListServ")
+}
+
+
+calcAxis <- function(metric, u){
+  eventType <- NA
+  if(metric=="Total"){
+    eventType <- colSums(u[, metricTypesc[2:length(metricTypesc)]])
+  }
+  year <- loopThrough(u, "Year", metric)
+
+  major <- loopThrough(u, "Major", metric)
+
+  return(list(eventType, year, major))
+}
+
+
+loopThrough <- function(u, crit, metric){
+  unis <- unique(u[[crit]])
+  mat <- data.frame(unis, 0)
+  colnames(mat) <- c(crit, metric)
+  for(i in 2:nrow(u)) {
+    if (is.na(u[[crit]][i])) {
+      mat[1, 2] = mat[1, 2] + u[[metric]][i]
+    } else {
+      for(j in 2:length(unis)) {
+        if (mat[j, 1] == u[[crit]][i]) {
+          mat[j, 2] = mat[j, 2] + u[[metric]][i]
+        }
+      }
+    }
+  }
+  return(mat)
+}
+
+
+createGraph <- function(gdata, type){
+    ggplot(gdata, aes(y=Total, x=event, fill=event)) + geom_bar(stat='identity')
+    return(ggplot(gdata, aes(y=Total, x=event, fill=event)) + geom_bar(stat='identity'))
+
+}
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$logo <- renderImage({
+    outfile <- tempfile(fileext-'.png')
+  })
     
   # on submission of intial URLs
     observeEvent(input$submit, {
@@ -126,25 +228,64 @@ server <- function(input, output) {
         
       } else {
         event <- event %>% mutate(type=input$eventType) %>% relocate(type, .before=Timestamp) %>% mutate(`UFID  (Ex: 12345678)`=as.character(`UFID  (Ex: 12345678)`))
-        writeMaster(input$workbook, event, "TA", input$eventType, "ex")
+        writeMaster(input$workbook, event, input$masterName, input$eventType, input$eventName)
         parseUniques(input$workbook, input$uniName, unique, event)
         master <- read_sheet(input$workbook, sheet=input$masterName)
-        master
       }
       
+      if(input$metrics == "Total"){
+        output$graph <- renderPlot({
+          results <- calcAxis(input$metrics, unique)
+          print("r")
+          r <- as.data.frame(results[1], col.names = list("Total"))
+          print("df")
+          r <- rownames_to_column(r, var="event")
+          print("rn")
+          print(input$metrics)
+          
+          ggplot(r, aes(y=Total, x=event, fill=event)) + geom_bar(stat='identity')
+        }, height=400, width=600)
+        
+      }
+      output$graph2 <- renderPlot({
+        results <- calcAxis(input$metrics, unique)
+        print("r")
+        r <- as.data.frame(results[2])
+        colnames(r) <- c("Year", "Number of Attendees")
+        print("df")
+        print("rn")
+        print(input$metrics)
+        
+        ggplot(r, aes(y=`Number of Attendees`, x=Year, fill=Year)) + geom_bar(stat='identity')  + ggtitle(paste0("Breakdown of Year by ", input$metrics, " Event"))
+      }, height=400, width=600)
+      
+      output$graph3 <- renderPlot({
+        results <- calcAxis(input$metrics, unique)
+        r <- as.data.frame(results[3])
+        colnames(r) <- c("Major", "Number of Attendees")
+
+        
+        ggplot(r, aes(y=`Number of Attendees`, x=Major, fill=Major)) + geom_bar(stat='identity') + ggtitle(paste0("Breakdown of Major by ", input$metrics, " Event"))
+      }, height=400, width=600)
+      
+      observeEvent(input$emails, {
+        master <- read_sheet(input$workbook, sheet=input$masterName)
+        emailList(master, input$workbook)
+      })
+      
+      observeEvent(input$memAmt, {
+        unique <- read_sheet(input$workbook, sheet=input$uniName)
+        tops <- topMem(input$memAmt, unique)
+        output$table <- renderTable(tops)
+      })
       
     })
   
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  
+  
+  
+  
 }
 
 # Run the application 
