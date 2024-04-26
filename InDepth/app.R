127c117a43170c5f0b64376804f037b899625492
eStrau <ellestrauss01@gmail.com>
2024-04-26 18:10:04 -0400

    Updating Final Report

--- InDepth/app.R
+++ InDepth/app.R
@@ -50,65 +50,68 @@ ui <- fluidPage(
   
   # Title Panel
   titlePanel(div(class='title-container', div(class = "logo", uiOutput("ourLogo")))),
-  tags$h3("Upload Attendance"),
-  # Upload Attendance Section
-  mainPanel(
-    fluidRow(
-      layout_columns(
-        textInput('workbook',"Enter Attendnace Workbook URL:")
-      )
-    ),
-    fluidRow(
-        layout_columns(
-                       textInput('masterName',"Enter Master Worksheet Name:"),
-                       textInput('eventName',"Enter Event Worksheet Name:"),
-                       
-        )
-    ),
-    fluidRow(
-      layout_columns(
-                     textInput('uniName',"Enter Unique Members Worksheet Name:"),
-                     textInput('errors',"Enter Error Worksheet Name:"),
-                     
-      )
-    ),
-    fluidRow(
-      layout_columns(
-        selectInput('eventType', "Select Event Type", choices=eventTypes),
-      )
-    ),
-    fluidRow(
-      actionButton("submit", "Submit!", width=300),
-    ),
-  ), 
-  br(), br(),
-  # ListServ Emails
-  mainPanel(
-    fluidRow(br(),tags$h3("ListServ Emails"),
-             actionButton("emails", "Create ListServ Email List"), br(),
-    ),
+  navset_card_underline(
+    nav_panel("Instructions", "How To (Add Instructions)"),
+    
+    nav_panel("General Fields", 
+              mainPanel(
+                fluidRow(
+                  layout_columns(
+                    textInput('workbook',"Enter Attendnace Workbook URL:")
+                  )
+                ),
+                fluidRow(
+                  layout_columns(
+                    textInput('masterName',"Enter Master Worksheet Name:"),
+                    textInput('uniName',"Enter Unique Members Worksheet Name:"),
+                    textInput('indiv',"Enter Individual Events Worksheet Name:"),
+                    textInput('errors',"Enter Error Worksheet Name:"),
+                  )
+                ),
+                fluidRow(
+                  actionButton("submit", "Submit!", width=300),
+                ),
+              )
+            ),
+    
+    nav_panel("Update Attendance", 
+                mainPanel(
+                  textInput('eventName',"Enter Event Worksheet Name:", width=500),
+                  selectInput('eventType', "Select Event Type", choices=eventTypes, width=500),
+                  actionButton("submit2", "Submit!", width=500),
+                )
+              ),
+    
+    nav_panel("Overall Metrics", 
+                mainPanel(
+                  fluidRow(
+                    tags$h3("Event Demographic Charts"),
+                    layout_columns(
+                      selectInput('metrics', "Select Metric Category", choices=metricTypes),
+                    ),
+                    plotOutput("graph"),
+                    plotOutput("graph2"),
+                    plotOutput("graph3"),
+                  ), 
+                )
+              ),
+    
+    nav_panel("Individual Event Metrics", 
+                mainPanel(
+                  layout_columns(
+                    selectInput('individual', "Select an Event", choices=c("empty for now")),
+                  ),
+                )
+              ),
+    
+    nav_panel("Top Member Results", 
+                mainPanel(
+                  tags$h3("Generate Top Members"),
+                  numericInput("memAmt", "Number of Top Members", 5, min=0), br(),
+                  tableOutput("table")
+                )
+              ),
   ),
-  br(), br(),
-  # Top Member Table
-  mainPanel(
-    fluidRow(tags$h3("Generate Top Members"),
-             numericInput("memAmt", "Number of Top Members", 5, min=0), br(),
-             tableOutput("table")
-    ),
-  ),
-  br(), br(),
-  # Event Demographics Graphs
-  mainPanel(
-    fluidRow(
-      tags$h3("Event Demographic Charts"),
-      layout_columns(
-        selectInput('metrics', "Select Metric Category", choices=metricTypes),
-      ),
-      plotOutput("graph"),
-      plotOutput("graph2"),
-      plotOutput("graph3"),
-    ), 
-  )
 )
 
 # FUNCTIONS
@@ -136,9 +139,13 @@ findErrors <- function(url, e, errName){
 
 # create Uniques sheet
 parseUniques <- function(url, uName, u, e){
+  print("entered parser")
   test <- data.frame(NA, NA, NA, NA, NA, 0, 0, 0, 0, 0, 0, 0)
+  print("test data set")
   colnames(test) <- uni_col
+  print("test columns set")
   range_write(url, test, sheet=uName, range = "A1", col_names =TRUE)
+  print("on to for loop")
   for(n in 1:nrow(e)){
     index <- which(u$`UFID (Ex: 12345678)`== e[[n, 'UFID  (Ex: 12345678)']])
     if(length(index) == 0){
@@ -176,9 +183,8 @@ parseUniques <- function(url, uName, u, e){
       member[e$type[[n]]] = member[e$type[[n]]] + 1
       range_write(url, member, sheet=uName, range=paste0("A",1+index[1]), col_names=FALSE)
     }
-    
   }
-  
+  print("for loop completed")
 }
 
 # top member
@@ -190,7 +196,6 @@ topMem <- function(amount, u){
 # email listserv
 emailList <- function(mast, url){
   colnames(mast) <- c("Event Type", "Timestamp", "First Name", "Last Name", "UFID", "Hear", "Return", "Email", "Pic", "Year", "Major")
-  
   emailList <- mast %>% filter(!is.na(Email)) %>% distinct(Email) %>% inner_join(mast, by = "Email") %>%  select(Email, `First Name`, `Last Name`) %>% distinct(Email, `First Name`, `Last Name`)
   #colnames(emailList) <- c("email", "first name", "last name")
   sheet_write(emailList, url, sheet="ListServ")
@@ -228,97 +233,143 @@ loopThrough <- function(u, crit, metric){
   return(mat)
 }
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  # globals in server that do not need to be rewritten each time server loads
+  initialSubmit <- reactiveVal(FALSE)
+  url <- reactiveVal("")
+  master <- reactiveVal(data.frame())
+  mName <- reactiveVal("")
+  unique <- reactiveVal(data.frame())
+  uName <- reactiveVal("")
+  errors <- reactiveVal("")
+  indName <- reactiveVal("")
+  indEvents <- reactiveVal(data.frame())
   
-  output$logo <- renderImage({
-    outfile <- tempfile(fileext-'.png')
+  # observing if general fields are good
+  observeEvent(input$submit, {
+    if (nchar(input$masterName) == 0 | nchar(input$uniName) == 0 | nchar(input$errors) == 0 | nchar(input$workbook) == 0 | nchar(input$indivName) == 0 ){ # any input is empty
+      showNotification("One or more of the fields are empty. Please ensure that are fields are completed in order to submit.", duration=15)
+      initialSubmit(FALSE)
+    }else if (!(input$masterName %in% sheet_names(input$workbook)) | !(input$uniName %in% sheet_names(input$workbook)) | !(input$errors %in% sheet_names(input$workbook)) | !(input$indivName %in% sheet_names(input$workbook))) { # ensures sheet name exists in the workbook
+      print("throw error")
+      showNotification("One or more of the sheet names you entered does not exist in the workbook. Please correct the enteries and retry.", duration=15)
+      initialSubmit(FALSE)
+    } else {
+      initialSubmit(TRUE)
+      print("correct sheet names")
+      # sets URL value
+      url(input$workbook)
+      # sets master sheet string name and data frame
+      mName(input$masterName)
+      master(read_sheet(url(), sheet=mName()))
+      # sets unique sheet string name and data frame
+      uName(input$uniName)
+      unique(read_sheet(url(), sheet=uName()))
+      # sets invalid UFID sheet string name
+      errors(input$errors)
+      # sets individual events sheet string name and dataframe
+      indName(input$indivName)
+      indEvents(read_sheet(url(), sheet=indName()))
+      # updates list serv
+      emailList(master(), url())
+      showNotification("ListServ has been updated!")
+    }
+    print(initialSubmit)
   })
-    
-  # on submission of intial URLs
-    observeEvent(input$submit, {
-      if (nchar(input$masterName) == 0 | nchar(input$eventName) == 0 | nchar(input$uniName) == 0 | nchar(input$errors) == 0 | nchar(input$workbook) == 0){ # any input is empty
-        showNotification("One or more of the fields are empty. Please ensure that are fields are completed in order to submit.", duration=15)
-      }else if (!(input$masterName %in% sheet_names(input$workbook)) | !(input$eventName %in% sheet_names(input$workbook)) | !(input$uniName %in% sheet_names(input$workbook)) | !(input$errors %in% sheet_names(input$workbook))) {
-        print("throw error")
-        showNotification("One or more of the sheet names you entered does not exist in the workbook. Please correct the enteries and retry.", duration=15)
-        
+  # observing if update attendance is called
+  observeEvent(input$submit2, {
+    print(url())
+    print("submit 2")
+    if (nchar(input$eventName) == 0) {
+      showNotification("Event Sheet Name is empty. Please ensure that the field are completed in order to submit.", duration=15)
+    } else if (initialSubmit()){
+      print(sheet_names(url()))
+      if(!(input$eventName %in% sheet_names(url()))) {
+        print("error 2")
+        showNotification("The sheet name entered for Event Sheet does not exist in your workbook. Please correct the enteries and retry.", duration=15)
       } else {
-        print("correct sheet names")
-        master <- read_sheet(input$workbook, sheet=input$masterName)
-        event <- read_sheet(input$workbook, sheet=input$eventName)
-        unique <- read_sheet(input$workbook, sheet=input$uniName)
-        if(findErrors(input$workbook, event, input$errors)){
+        event <- read_sheet(url(), sheet=input$eventName)
+        if(findErrors(url(), event, errors())){
           showNotification("There are errors with your UFIDs! Please correct them and retry. UFIDs should contain only numeric values and be 8 digits long.", duration=10)
-          
         } else {
           event <- event %>% mutate(type=input$eventType) %>% relocate(type, .before=Timestamp) %>% mutate(`UFID  (Ex: 12345678)`=as.character(`UFID  (Ex: 12345678)`))
-          writeMaster(input$workbook, event, input$masterName, input$eventType, input$eventName)
-          parseUniques(input$workbook, input$uniName, unique, event)
-          master <- read_sheet(input$workbook, sheet=input$masterName)
+          writeMaster(url(), event, mName(), input$eventType, input$eventName)
+          parseUniques(url(), uName(), unique(), event)
+          master(read_sheet(url(), sheet=mName()))
+          emailList(master(), url())
+          showNotification("ListServ has been updated!")
         }
-        
-        if(input$metrics == "Total"){
-          output$graph <- renderPlot({
-            results <- calcAxis(input$metrics, unique)
-            print("r")
-            r <- as.data.frame(results[1], col.names = list("Total"))
-            print("df")
-            r <- rownames_to_column(r, var="event")
-            print("rn")
-            print(input$metrics)
-            
-            ggplot(r, aes(y=Total, x=event, fill=event)) + geom_bar(stat='identity')
-          }, height=400, width=600)
-          
-        }
-        output$graph2 <- renderPlot({
-          results <- calcAxis(input$metrics, unique)
+      }
+    } else {
+      print("error 1")
+      showNotification("Error: General Fields have not be filled out saved.", duration=10)
+    }
+  })
+  
+  # graphs for overall metrics
+  observe({
+    if (initialSubmit()){
+      if(input$metrics == "Total"){
+        output$graph <- renderPlot({
+          results <- calcAxis(input$metrics, unique())
           print("r")
-          r <- as.data.frame(results[2])
-          colnames(r) <- c("Year", "Number of Attendees")
+          r <- as.data.frame(results[1], col.names = list("Total"))
           print("df")
+          r <- rownames_to_column(r, var="event")
           print("rn")
           print(input$metrics)
           
-          ggplot(r, aes(y=`Number of Attendees`, x=Year, fill=Year)) + geom_bar(stat='identity')  + ggtitle(paste0("Breakdown of Year by ", input$metrics, " Event"))
+          ggplot(r, aes(y=Total, x=event, fill=event)) + geom_bar(stat='identity')
         }, height=400, width=600)
         
-        output$graph3 <- renderPlot({
-          results <- calcAxis(input$metrics, unique)
-          r <- as.data.frame(results[3])
-          colnames(r) <- c("Major", "Number of Attendees")
-          
-          
-          ggplot(r, aes(y=`Number of Attendees`, x=Major, fill=Major)) + geom_bar(stat='identity') + ggtitle(paste0("Breakdown of Major by ", input$metrics, " Event"))
-        }, height=400, width=600)
-        
-        observeEvent(input$emails, {
-          master <- read_sheet(input$workbook, sheet=input$masterName)
-          if (nrow(master) != 0) { # checks if master has any inputs
-            emailList(master, input$workbook)
-          } else {
-            showNotification("Your Master Sheet is Empty.", duration=15)
-            
-          }
-          
-        })
-        
-        observeEvent(input$memAmt, {
-          unique <- read_sheet(input$workbook, sheet=input$uniName)
-          tops <- topMem(input$memAmt, unique)
-          output$table <- renderTable(tops)
-        })
       }
-    })
-    output$logo <- renderImage({
-      filename <- normalizePath(file.path('./', paste0('InDepthLogo', '.png', sep='')))
-      list(src=filename, width=200, height=200, alt="Logo")}, 
-      deleteFile = FALSE
-    )
-    output$ourLogo <- renderUI({
-      imageOutput("logo", width = "200px")
-    })
+      output$graph2 <- renderPlot({
+        results <- calcAxis(input$metrics, unique())
+        print("r")
+        r <- as.data.frame(results[2])
+        colnames(r) <- c("Year", "Number of Attendees")
+        print("df")
+        print("rn")
+        print(input$metrics)
+        
+        ggplot(r, aes(y=`Number of Attendees`, x=Year, fill=Year)) + geom_bar(stat='identity')  + ggtitle(paste0("Breakdown of Year by ", input$metrics, " Event"))
+      }, height=400, width=600)
+      
+      output$graph3 <- renderPlot({
+        results <- calcAxis(input$metrics, unique())
+        r <- as.data.frame(results[3])
+        colnames(r) <- c("Major", "Number of Attendees")
+        
+        
+        ggplot(r, aes(y=`Number of Attendees`, x=Major, fill=Major)) + geom_bar(stat='identity') + ggtitle(paste0("Breakdown of Major by ", input$metrics, " Event"))
+      }, height=400, width=600)
+    }
+  })
+  
+  
+  
+  
+  observeEvent(input$memAmt, {
+    if (initialSubmit()){
+      unique(read_sheet(url(), sheet=uName()))
+      tops <- topMem(input$memAmt, unique())
+      output$table <- renderTable(tops)
+    } else {
+      showNotification("Error: General Fields has not be entered or saved to proceed", duration=15)
+    }
+  })
+  
+  # logo renderings
+  output$logo <- renderImage({
+    filename <- normalizePath(file.path('./', paste0('InDepthLogo', '.png', sep='')))
+    list(src=filename, width=200, height=200, alt="Logo")}, 
+    deleteFile = FALSE
+  )
+  output$ourLogo <- renderUI({
+    imageOutput("logo", width = "200px")
+  })
 }
 
 # Run the application 
